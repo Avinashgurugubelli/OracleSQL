a425fdf6c2022d09dedc8bb1b83c743bdd7c19ca
avinash <avinash.gurugubelli@philips.com>
2019-06-21 22:43:28 +0530

    documentation added for 01_CreateTables_and_insertion.sql

--- Notes/01_CreateTables_and_insertion.sql
+++ Notes/01_CreateTables_and_insertion.sql
@@ -1,4 +1,46 @@
+/*
+CREATE TABLE statement is used to create a table in a database. 
+    - Database tables are organized into rows and columns. 
+    - Each table must have a name and can have any number of columns (minimum 1 column is required). 
+    - Each column must have a data type which determines the type of values that can be stored. 
+    - CREATE TABLE command will fail if a table is already existing in the database with same name. 
+    - All tables must have a unique name.
+    Syntax:
+        create TABLE TABLE_NAME(
+        column_name data_type [constraint],
+        second_column_name data_type [constraint],
+        );
+--------------------------------------------------------------------------------------------------------
+DROP TABLE statement is used to remove an existing table from the database
+    - Syntax: DROP table table_name;
+---------------------------------------------------------------------------------------------------------
+Constraints:
+************
+- Constraints are the rules enforced on the data columns of a table. 
+- These are used to limit the type of data that can go into a table. This ensures the accuracy and reliability of the data in the database
+- These constraints are typically specified along with the CREATE TABLE statement.
+- Constraints are classified into multiple types based on the number of columns they act upon as well as on the way they are specified.
+- Table level constraint can be specified after all columns used in the constraint have been defined. 
+- It is not necessary to specify them after all columns in the table are defined. Composite constraints can only be specified as table level constraints.
+- Various constraints that can be created on database tables are:
 
+        NOT NULL: NOT NULL Constraint prevents a column from accepting NULL values. NOT NULL can only be applied as a column level constraint.
+        Constraint name is optional and it can be specified by using CONSTRAINT keyword
+
+        PRIMARY KEY: PRIMARY KEY constraint on a column ensures that the column cannot contain NULL and duplicate values. We can have only one PRIMARY KEY in a table.
+        
+        CHECK: CHECK constraint is used to limit the values that can be specified for a column.
+
+        UNIQUE: UNIQUE constraint on a column ensures that two rows in a table cannnot have same value in that column. 
+        Unlike Primary Key, UNIQUE constraint allows NULL values. A table can have many UNIQUE constraints.
+
+        FOREIGN KEY:A foreign key is a set of one or more columns in the child table whose values are required to match with corresponding columns in the parent table.
+         - Foreign key establishes a relationship between these two tables.
+         - Foreign key columns on child tables must be primary key or unique on the parent table. 
+
+---------------------------------------------------------------------------------------------------------------------------
+We can also specify DEFAULT value for a column. Oracle database does not consider DEFAULT as a constraint
+*/
 create TABLE STUDENTS(
     ID INT PRIMARY KEY NOT NULL,
     FIRST_NAME VARCHAR(20) NOT NULL,
